df48c1e8155af60671179a1e6abf753db8138ea6
Victor Manuel Martinez Cruz <vicmarc30@gmail.com>
2025-06-24 12:07:34 -0600

    feat: implement patient loading functionality and display in data table

--- src/app/dashboard/especialista/pacientes/page.tsx
+++ src/app/dashboard/especialista/pacientes/page.tsx
@@ -1,7 +1,42 @@
+'use client'
+
+import { DataTable } from '@/components/data-table'
+import createClientSupabase from '@/lib/supabase/client'
+import { useUser } from '@clerk/nextjs'
+import { useEffect, useState } from 'react'
+
 export default function Page() {
+  const [patients, setPatients] = useState<any[]>([])
+  const [loading, setLoading] = useState(true)
+
+  // Cargar usuario con clerk
+  const { user } = useUser()
+
+  // Crear cliente de supabase
+  const client = createClientSupabase()
+
+  // Cargar pacientes desde la base de datos
+  useEffect(() => {
+    if (!user) return
+
+    async function loadPatients() {
+      setLoading(true)
+      const { data, error } = await client.from('patients').select()
+
+      if (!error) setPatients(data)
+      setLoading(false)
+    }
+    loadPatients()
+  }, [user])
+
+  // Mostrar mensaje de carga
+  if (loading) {
+    return <p className="text-center text-gray-500">Cargando pacientes...</p>
+  }
+
   return (
-    <div>
-      <h1>Pacientes</h1>
-    </div>
+    <>
+      <DataTable data={patients} />
+    </>
   )
 }
